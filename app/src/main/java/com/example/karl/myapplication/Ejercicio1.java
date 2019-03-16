4e0a272e7c9c4ea45f905df910f62334098f10e1
CarlosBeltranP <00507511@uca.edu.sv>
2019-03-16 02:54:52 -0600

    ya casi

---
+++ app/src/main/java/com/example/karl/myapplication/Ejercicio1.java
@@ -0,0 +1,70 @@
+package com.example.karl.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Ejercicio1 extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button boton;
+    private TextView text;
+
+    private  String texto;
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            normalClick();
+        }
+    };
+
+    private View.OnLongClickListener clickListeneeeer = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            if(v.getId()==R.id.boton){
+                longClick();
+            }
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        boton = findViewById(R.id.boton);
+        boton.setOnClickListener(clickListener);
+        boton.setOnLongClickListener(clickListeneeeer);
+
+        text = findViewById(R.id.text);
+
+        normalClick();
+
+    }
+
+    private void  normalClick(){
+        texto = username.getText().toString();
+        if(texto!=""){
+            text.setText(texto);
+        }
+        else{
+            text.setText("");
+        }
+
+    }
+
+    private void longClick(){
+        texto = password.getText().toString();
+        if(texto!=""){
+            text.setText(texto);
+        }
+
+    }
+}
